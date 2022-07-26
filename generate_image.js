101ad7fdcd39302c409e2681bae8065efdccc726
Jack Blake <78292128+JackBlake-zkq@users.noreply.github.com>
2022-07-25 22:07:59 -0400

    init with Action setup

---
+++ generate_image.js
@@ -0,0 +1,27 @@
+import OpenAI from "openai"
+import fs from "fs"
+import axios from "axios"
+
+const start = "<!-- start prompt -->"
+const end = "<!-- end prompt -->"
+
+const raw = fs.readFileSync("./README.md").toString()
+
+const for_prompt = raw.slice(raw.indexOf(start) + start.length, raw.indexOf(end)).trim()
+
+const openai = new OpenAI()
+
+const url = (await openai.images.generate({ 
+    prompt: "Generate a graphic with no text to represent someone with this GitHub Bio:\n\n" + for_prompt,
+    model: "dall-e-3",
+})).data[0].url
+
+axios({ url, responseType: 'stream' }).then(response =>
+    new Promise((resolve, reject) => {
+        response.data
+            .pipe(fs.createWriteStream("dalle-vis.png"))
+            .on('finish', () => resolve())
+            .on('error', e => reject(e));
+    }),
+);
+
